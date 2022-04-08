b63c397168d80af9b711f6e4b3c50a09dc508d87
Pranay Singh <pranay.singh.1393@gmail.com>
2022-04-08 22:49:17 +0530

    Stream API Filter Map Reduce Example
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

--- Stream_API/src/com/gk/streamapi/map_reduce/Filter_Map_Reduce_Example.java
+++ Stream_API/src/com/gk/streamapi/map_reduce/Filter_Map_Reduce_Example.java
@@ -1,5 +1,11 @@
 package com.gk.streamapi.map_reduce;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 //Evaluate average salary of grade A employees
 public class Filter_Map_Reduce_Example {
 
@@ -14,6 +20,16 @@ public class Filter_Map_Reduce_Example {
 				.average().getAsDouble();
 		System.out.println(averageSalaryGradeAEmp);// 850000.0
 
+		List<Integer> list1 = Arrays.asList(1, 2, 3);
+		List<Integer> list2 = Arrays.asList(4, 5, 6);
+		List<Integer> list3 = Arrays.asList(7, 8, 9);
+
+		int list1sum = list1.stream().reduce(Integer::sum).get();
+		int list2sum = list2.stream().reduce(Integer::sum).get();
+		int list3sum = list3.stream().reduce(Integer::sum).get();
+
+		int maximumSumOfAllList = Arrays.asList(list1sum, list2sum, list3sum).stream().mapToInt(i -> i).max().getAsInt();
+		System.out.println(maximumSumOfAllList);
 	}
 
 }
