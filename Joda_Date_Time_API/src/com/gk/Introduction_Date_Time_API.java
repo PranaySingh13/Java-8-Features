d198d515967c46096970ec4bb7d5bf30c62de135
PranaySingh13 <pranay.singh.1393@gmail.com>
2023-02-03 03:42:46 +0530

    Introduction new Date Time API in JAVA 8
    Signed-off-by: PranaySingh13 <pranay.singh.1393@gmail.com>

---
+++ Joda_Date_Time_API/src/com/gk/Introduction_Date_Time_API.java
@@ -0,0 +1,22 @@
+package com.gk;
+
+/**
+ * Java 8 introduced new APIs for Date and Time to address the shortcomings of
+ * the older java.util.Date and java.util.Calendar.
+ * 
+ * Core APIs of the new Java 8 that are part of the java.time package like
+ * LocalDate,LocalTime,LocalDateTime,ZonedDateTime,Period,Duration and their
+ * supported APIs.
+ * 
+ * Difference between Old and new Java 8 Date/Time APIs
+ * 
+ * Thread Safety.
+ * 
+ * APIs design and understanding
+ * 
+ * Timezone handling
+ *
+ */
+public class Introduction_Date_Time_API {
+
+}
